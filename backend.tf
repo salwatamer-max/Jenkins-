df6b8a4eaba8711fe3f6b685a10496d59060ccc5
salwatamer-max <salwatamer8901@gmail.com>
2025-11-14 20:41:52 +0200

    Change S3 backend region from eu-east-1 to us-east-1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "salwa-jenkins-bucket"
     key          = "eks/terraform.tfstate"
-    region       = "eu-east-1"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
   }
